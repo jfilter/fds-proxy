cd46e1cd49ab0da9101bdf6bbaf4ba0b5af21469
Johannes Filter <hi@jfilter.de>
2018-04-12 22:55:58 +0200

    remove debug prints

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const app = express();
 const API_URL = 'https://fragdenstaat.de';
 
 app.use('/', function(req, res) {
-  console.log(req.url);
-  console.log(req.path);
-  console.log(req.headers);
   const url = API_URL + req.path;
-  console.log(url);
   req.pipe(request(url)).pipe(res);
 });
 
