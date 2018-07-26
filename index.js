d77ea171467670ccb8a1132477cdd83f493e3cdc
Johannes Filter <hi@jfilter.de>
2018-07-26 17:00:52 +0200

    use full url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const API_URL = "https://fragdenstaat.de";
 app.use(cors());
 
 app.use("/", function(req, res) {
-  const url = API_URL + req.path;
+  const url = API_URL + req.url;
   req.pipe(request(url)).pipe(res);
 });
 
