f73f8263ae4ac0538a0ab478487b352be3d22731
Johannes Filter <hi@jfilter.de>
2018-07-26 16:53:12 +0200

    add cors

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const request = require('request');
+const express = require("express");
+const request = require("request");
+const cors = require("cors");
 
 const app = express();
-const API_URL = 'https://fragdenstaat.de';
+const API_URL = "https://fragdenstaat.de";
 
-app.use('/', function(req, res) {
+app.use(cors());
+
+app.use("/", function(req, res) {
   const url = API_URL + req.path;
   req.pipe(request(url)).pipe(res);
 });
-
 
 app.listen(5000);
